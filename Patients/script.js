6e606928e60892ebe7d90b65427ec0b7e0e4d342
Vinicius C. Fabris <cencifabris@gmail.com>
2023-11-28 22:48:54 -0300

    Cria tela de pacientes

--- Patients/script.js
+++ Patients/script.js
@@ -1,49 +1,10 @@
-let pacientes = [];
-let pacienteId = 1;
+const pacienteBtn = document.getElementById('pacienteBtn');
 
-function abrirFormulario() {
-    const formulario = document.getElementById("formulario");
-    formulario.classList.remove("escondido");
-}
-
-function adicionarPaciente() {
-    const nome = document.getElementById("nome").value;
-
-    // Adicione os demais campos do paciente conforme necessário
-
-    const novoPaciente = {
-        id: pacienteId++,
-        nome: nome,
-        // Adicione os demais campos do paciente conforme necessário
-    };
-
-    pacientes.push(novoPaciente);
-    exibirPacientes();
-    limparFormulario();
-}
-
-function exibirPacientes() {
-    const tabelaPacientes = document.getElementById("tabela-pacientes");
-    tabelaPacientes.innerHTML = "";
-
-    pacientes.forEach((paciente) => {
-        const row = tabelaPacientes.insertRow();
-        for (const key in paciente) {
-            if (paciente.hasOwnProperty(key)) {
-                const cell = row.insertCell();
-                cell.appendChild(document.createTextNode(paciente[key]));
-            }
-        }
-    });
-}
-
-function limparFormulario() {
-    const formulario = document.getElementById("formulario");
-    formulario.classList.add("escondido");
-
-    // Limpar os campos do formulário aqui
-    document.getElementById("nome").value = "";
-}
-
-// Inicializa exibição da lista de pacientes
-exibirPacientes();
+pacienteBtn.addEventListener('click', () => {
+    const div = document.querySelector('.form_box')
+    if (div.classList.contains('esconder')) {
+        div.classList.remove('esconder')
+    } else {
+    div.classList.add('esconder')
+    }
+})
